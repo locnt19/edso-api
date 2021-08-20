ada411b3a1e7677376b305806231fae0e35383e2
locnt19 <locnt19@gmail.com>
2021-08-20 18:19:18 +0700

    get center with filter

--- src/graphql/queries/Center.js
+++ src/graphql/queries/Center.js
@@ -1,4 +1,5 @@
 import { CenterModel } from '../../models/Center';
+import escapeStringRegexp from 'escape-string-regexp';
 
 export const getCenter = async (parent, args, context, info) => {
     try {
@@ -9,10 +10,39 @@ export const getCenter = async (parent, args, context, info) => {
             limit: paginate && paginate.limit ? paginate.limit : 10
         };
 
-        const data = await CenterModel.paginate({}, options);
+        const filterConvert = filter && changeObjectToDotNotationFormat(filter);
+
+        if (filterConvert) {
+            for (const key in filterConvert) {
+                if (Object.hasOwnProperty.call(filterConvert, key)) {
+                    const element = filterConvert[key];
+                    const $regex = escapeStringRegexp(element);
+
+                    filterConvert[key] = { $regex: new RegExp($regex, 'i') };
+                }
+            }
+        }
+
+        const data = await CenterModel.paginate(filterConvert, options);
 
         return data;
     } catch (error) {
         return error;
     }
 };
+
+function changeObjectToDotNotationFormat(inputObject, current, prefinalObject) {
+    const result = prefinalObject ?? {};
+
+    for (const key in inputObject) {
+        const value = inputObject[key];
+        const newKey = current ? `${current}.${key}` : key;
+        if (value && typeof value === 'object') {
+            changeObjectToDotNotationFormat(value, newKey, result);
+        } else {
+            result[newKey] = value;
+        }
+    }
+
+    return result;
+}
