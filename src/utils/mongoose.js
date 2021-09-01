87b21ac3797126b9bc2110f458a3096b1fe91ceb
Hung Nguyen <hung.a.nguyen19@gmail.com>
2021-09-01 19:45:17 +0700

    change mongoose connection

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const DB_HOST = process.env.DB_HOST || 'localhost',
+const DB_HOST = process.env.DB_HOST || 'hnguyen.qaysn.mongodb.net',
     DB_PORT = process.env.DB_PORT || '27017',
-    DB_NAME = process.env.DB_NAME || 'backend',
-    DB_USERNAME = process.env.DB_USERNAME || '',
-    DB_PASSWORD = process.env.DB_PASSWORD || '',
+    DB_NAME = process.env.DB_NAME || 'test',
+    DB_USERNAME = process.env.DB_USERNAME || 'edsoadmin',
+    DB_PASSWORD = process.env.DB_PASSWORD || 'qwerty123456',
     LOGIN_DB = DB_USERNAME && DB_PASSWORD ? `${DB_USERNAME}:${DB_PASSWORD}@` : '',
-    DB_USE_SRV = process.env.DB_USE_SRV || 'false';
+    DB_USE_SRV = process.env.DB_USE_SRV || 'true';
 
 mongoose.connection.on('open', () => console.log('🚀 Mongo DB connected'));
 
